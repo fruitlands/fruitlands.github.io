47fbe0ac34d0f2c493a9da86d6999db23a5a1ec8
fruitlands <31262408+fruitlands@users.noreply.github.com>
2017-10-26 09:29:31 +1300

    Add files via upload

---
+++ js/buttons.js
@@ -0,0 +1,12 @@
+function homeButtonClick(){
+  location.href = "index.html";
+  this.blur();
+}
+
+function historyButtonClick(){
+  location.href = "history.html";
+}
+
+function infoButtonClick(){
+  location.href = "info.html";
+}
